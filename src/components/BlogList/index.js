784897b5fa14c5bcbb4ca437971ba5bdc91c1caf
Anil Kumar <mallula9010@gmail.com>
2021-05-28 18:02:28 +0530

    Updated and concepts Revised

--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,47 +1,47 @@
-import {Component} from 'react'
-import Loader from 'react-loader-spinner'
+import { Component } from "react";
+import Loader from "react-loader-spinner";
 
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-import BlogItem from '../BlogItem'
+import BlogItem from "../BlogItem";
 
-import './index.css'
+import "./index.css";
 
 class BlogList extends Component {
-  state = {isLoading: true, blogsData: []}
+  state = { isLoading: true, blogsData: [] };
 
   componentDidMount() {
-    this.getBlogsData()
+    this.getBlogsData();
   }
 
   getBlogsData = async () => {
-    const response = await fetch('https://apis.ccbp.in/blogs')
-    const data = await response.json()
-    const formattedData = data.map(eachItem => ({
+    const response = await fetch("https://apis.ccbp.in/blogs");
+    const data = await response.json();
+    const formattedData = data.map((eachItem) => ({
       id: eachItem.id,
       title: eachItem.title,
       imageUrl: eachItem.image_url,
       avatarUrl: eachItem.avatar_url,
       author: eachItem.author,
       topic: eachItem.topic,
-    }))
-    this.setState({blogsData: formattedData, isLoading: false})
-  }
+    }));
+    this.setState({ blogsData: formattedData, isLoading: false });
+  };
 
   render() {
-    const {blogsData, isLoading} = this.state
+    const { blogsData, isLoading } = this.state;
     return (
-      <div className="blog-list-container">
+      <div className='blog-list-container'>
         {isLoading ? (
-          <div testid="loader">
-            <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+          <div testid='loader'>
+            <Loader type='TailSpin' color='#00BFFF' height={50} width={50} />
           </div>
         ) : (
-          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+          blogsData.map((item) => <BlogItem blogData={item} key={item.id} />)
         )}
       </div>
-    )
+    );
   }
 }
 
-export default BlogList
+export default BlogList;
--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,15 +1,15 @@
-import './index.css'
+import "./index.css";
 
 const UserInfo = () => (
-  <div className="user-info-container">
+  <div className='user-info-container'>
     <img
-      className="profile-pic"
-      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
-      alt="profile pic"
+      className='profile-pic'
+      src='https://assets.ccbp.in/frontend/react-js/profile-img.png'
+      alt='profile pic'
     />
-    <h1 className="user-name">Wade Warren</h1>
-    <p className="user-designation">Software developer at UK</p>
+    <h1 className='user-name'>Wade Warren</h1>
+    <p className='user-designation'>Software developer at UK</p>
   </div>
-)
+);
 
-export default UserInfo
+export default UserInfo;
